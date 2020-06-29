ad3ae7a53ae6c67f1fbb3996b6205ce73fd9e6d0
Oskari Pohja <oskupohja@hotmail.com>
2020-06-30 00:28:26 +0300

    Yolo filelists are now created. Train-test split is now a paremeter in to_yolo -script

--- auairtools/to_yolo.py
+++ auairtools/to_yolo.py
@@ -1,12 +1,14 @@
 import os
 import json
-
+import time
+import math
+import random
 from absl import app, flags, logging
 from absl.flags import FLAGS
 
-
 flags.DEFINE_string('annotations', './data/annotations.json','Path to annotations json-file')
 flags.DEFINE_string('output_dir', './data/yololabels', 'Output location for label files.')
+flags.DEFINE_string('test_split','0.15','Portion of data used for test data.')
 
 def to_rows(annotation):
     
@@ -52,30 +54,32 @@ def main(_argv):
     logging.info("Class mapping loaded: %s", annotations["categories"])
 
     file_list = annotations["annotations"]
-    file_count = len(file_list)
+    random.shuffle(file_list)
+    first_test_index = int(math.ceil(len(file_list)*(1-float(FLAGS.test_split))))
 
-    with open(os.path.join(FLAGS.output_dir,'auair.names'), 'w') as f:
+    with open(os.path.join(FLAGS.output_dir,'obj.names'), 'w') as f:
         for item in annotations["categories"]:
             f.write("%s\n" % item)
 
     print("Exporting, please wait...")
     
-    for annotated_file in file_list:
+    for idx, annotated_file in enumerate(file_list):
         im_name, labels = to_rows(annotated_file)
-        print(im_name)
-        with open(os.path.join(FLAGS.output_dir,im_name.replace(".jpg",".txt")), 'a') as f:
+        if idx < first_test_index:
+           file_list_filename = "train.txt"
+        else:
+            file_list_filename = "test.txt"
+        with open(os.path.join(FLAGS.output_dir,file_list_filename), "a") as f:
+            f.write("data/obj/{}\n".format(im_name))
+        print("{}: {}".format(idx, im_name))
+        with open(os.path.join(FLAGS.output_dir,"obj",im_name.replace(".jpg",".txt")), 'a') as f:
             for labelr in labels:
                 f.write("{} {} {} {} {}\n".format(
                     labelr["class_id"],
                     labelr["x_center"],
                     labelr["y_center"],
                     labelr["width"],
-                    labelr["height"]
-                ))
-
-
-
-    logging.info("Done")
+                    labelr["height"]))
 
 
 if __name__ == '__main__':
